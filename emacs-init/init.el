0d398b6044f15fb7f20eea9d8e549fc4d15f8ca3
unknown <your@email.example.com>
2024-07-05 10:09:39 -0700

    add initial emacs config in org

--- emacs-init/init.el
+++ emacs-init/init.el
@@ -12,3 +12,9 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+
+;;
+;; REAL INIT:
+(require 'org)
+(org-babel-load-file (expand-file-name "./emacs-init/init-config.org"))
+
