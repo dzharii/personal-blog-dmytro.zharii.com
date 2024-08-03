205796a490ddc600e431517259a136d0468b0d66
Gogs <you@example.com>
2024-08-03 14:32:27 -0700

    add org download

--- emacs-init/init.el
+++ emacs-init/init.el
@@ -5,7 +5,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(lsp-ui lsp-mode eshell-toggle esh-toggle markdown-mode ibuffer-sidebar scala-mode nyan-mode use-package)))
+   '(org-download lsp-ui lsp-mode eshell-toggle esh-toggle markdown-mode ibuffer-sidebar scala-mode nyan-mode use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
