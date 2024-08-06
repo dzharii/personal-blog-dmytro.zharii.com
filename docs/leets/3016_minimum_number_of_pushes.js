6d97b3dff4de498dda02cccfaa6e4cadaf9d80e3
Gogs <you@example.com>
2024-08-06 16:52:20 -0700

    update Tue Aug  6 04:52:20 PM PDT 2024

--- docs/leets/3016_minimum_number_of_pushes.js
+++ docs/leets/3016_minimum_number_of_pushes.js
@@ -7,9 +7,25 @@ function minimumPushes(word) {
     const log = typeof NestedInteger !== 'undefined' ? () => {} : console.log;
     const table = typeof NestedInteger !== 'undefined' ? () => {} : console.table;
 
-    totalPushes = 0;
+    const f = {};
+    for (const char of word) {
+        f[char] = (f[char] || 0) + 1;
+    }
 
-    return totalPushes;
+    log(JSON.stringify(f))
+
+    const sf = Object.entries(f)
+          .sort(([, a], [, b]) => b - a);
+
+    log(JSON.stringify(sf))
+
+    let pushes = 0;
+    for (let i = 0; i < sf.length; i++) {
+        const [ch, freq] = sf[i];
+        pushes += freq * (Math.floor(i / 8) + 1);
+    }
+
+    return pushes;
 }
 
 // Test cases
