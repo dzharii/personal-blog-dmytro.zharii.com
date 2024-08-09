e7de61fb17fd6c93285392f3f12866c65518b75a
unknown <your@email.example.com>
2024-08-09 15:31:05 -0700

    update Fri 08/09/2024

--- src/leets/840_magic_squares_in_grid.js
+++ src/leets/840_magic_squares_in_grid.js
@@ -7,7 +7,60 @@ function numMagicSquaresInside(grid) {
 
     const log = typeof NestedInteger !== 'undefined' ? () => {} : console.log;
     const table = typeof NestedInteger !== 'undefined' ? () => {} : console.table;
+
+    const isMagicSquare = (r, c) => {
+        const s = new Set();
+        const nums = [];
+
+        // unique 1 .. 9
+        for (let i = r; i < r + 3; i++) {
+            for (let j = c; j < c + 3; j++) {
+                const num = grid[i][j];
+                nums.push(num);
+                if (num < 1 || num > 9 || s.has(num)) {
+                    return false;
+                }
+                s.add(num);
+            }
+        }
+
+        const magicSum = 15;
+
+        // all rows and cols
+        for (let i = 0; i < 3; i++) {
+            const rowSum = grid[r + i][c] + grid[r + i][c + 1] + grid[r + i][c + 2];
+            if (rowSum !== magicSum) {
+                return false;
+            }
+
+            const colSum = grid[r][c + i] + grid[r + 1][c + i] + grid[r + 2][c + i];
+            if (colSum !== magicSum) {
+                return false;
+            }
+        }
+
+        // diagonalss
+        const diag1 = grid[r][c] + grid[r + 1][c + 1] + grid[r + 2][c + 2];
+        if (diag1 !== magicSum) {
+            return false;
+        }
+
+        const diag2 = grid[r + 2][c] + grid[r + 1][c + 1] + grid[r][c + 2];
+        if (diag2 !== magicSum) {
+            return false;
+        }
+
+
+        return true;
+    };
+
     let count = 0;
+
+    for (let r = 0; r <= grid.length - 3; r++) {
+        for (let c = 0; c <= grid[0].length - 3; c++) {
+            if (isMagicSquare(r, c)) count++;
+        }
+    }
 
     return count;
 }
